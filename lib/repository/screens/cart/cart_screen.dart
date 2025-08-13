300de0db7970fe23a5db86ccc8650f9692552307
Shahzalal <Shahzalalkhan@gmail.com>
2025-08-13 15:54:29 +0600

    Add cart screen ui

--- lib/repository/screens/cart/cart_screen.dart
+++ lib/repository/screens/cart/cart_screen.dart
@@ -1,13 +1,195 @@
+import 'package:blinkit_app/repository/widget/uihelper.dart';
 import 'package:flutter/material.dart';
 
 class CartScreen extends StatelessWidget {
-  const CartScreen({super.key});
+  final TextEditingController searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Cart Screen'),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              height: 190,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Color(0xFFF7CB45),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: Stack(
+                children: [
+                  Column(
+                    children: [
+                      const SizedBox(height: 30),
+                      Row(
+                        children: [
+                          const SizedBox(width: 20),
+                          UiHelper.customText(
+                            text: 'Blinkit in',
+                            color: Color(0xFF000000),
+                            fontweight: FontWeight.bold,
+                            fontsize: 14,
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const SizedBox(width: 20),
+                          UiHelper.customText(
+                            text: '16 Minutes',
+                            color: Color(0xFF000000),
+                            fontweight: FontWeight.bold,
+                            fontsize: 20,
+                          ),
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          const SizedBox(width: 20),
+                          UiHelper.customText(
+                            text: 'Home ',
+                            color: Color(0xFF000000),
+                            fontweight: FontWeight.bold,
+                            fontsize: 18,
+                          ),
+                          UiHelper.customText(
+                            text: '- Shahzalal, Usti Bazar, Naogaon, Dhaka',
+                            color: Color(0xFF000000),
+                            fontweight: FontWeight.normal,
+                            fontsize: 14,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  Positioned(
+                    right: 30,
+                    bottom: 100,
+                    child: CircleAvatar(
+                      radius: 18,
+                      backgroundColor: Colors.white,
+                      child: Icon(Icons.person, color: Colors.black),
+                    ),
+                  ),
+                  Positioned(
+                    left: 20,
+                    bottom: 20,
+                    child: UiHelper.customTextField(searchController),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(height: 20),
+            UiHelper.customImage(img: 'shopping-cart (1) 1.png'),
+            const SizedBox(height: 20),
+            UiHelper.customText(
+              text: 'Reordering will be easy',
+              color: Color(0xFF000000),
+              fontweight: FontWeight.bold,
+              fontsize: 16,
+            ),
+            const SizedBox(height: 5),
+            UiHelper.customText(
+              text: 'Items you order will show up here so you can buy',
+              color: Color(0xFF000000),
+              fontweight: FontWeight.normal,
+              fontsize: 12,
+            ),
+            UiHelper.customText(
+              text: 'them again easily.',
+              color: Color(0xFF000000),
+              fontweight: FontWeight.normal,
+              fontsize: 12,
+            ),
+            const SizedBox(height: 40),
+            Row(
+              children: [
+                const SizedBox(width: 20),
+                UiHelper.customText(
+                  text: 'Bestsellers',
+                  color: Color(0xFF000000),
+                  fontweight: FontWeight.bold,
+                  fontsize: 16,
+                ),
+              ],
+            ),
+            const SizedBox(height: 15),
+            Row(
+              children: [
+                const SizedBox(width: 20),
+                Column(
+                  children: [
+                    Stack(
+                      children: [
+                        UiHelper.customImage(img: 'milk.png'),
+                        Padding(
+                          padding: EdgeInsets.only(top: 95, left: 65),
+                          child: UiHelper.customButton(() {}),
+                        ),
+
+                      ],
+                    ),
+                    UiHelper.customText(
+                      text: 'Amul Taaza Toned',
+                      color: Color(0xFF000000),
+                      fontweight: FontWeight.normal,
+                      fontsize: 10,
+                    ),
+
+
+                  ],
+                ),
+                const SizedBox(width: 20),
+
+                Column(
+                  children: [
+                    Stack(
+                      children: [
+                        UiHelper.customImage(img: 'potato.png'),
+                        Padding(
+                          padding: EdgeInsets.only(top: 95, left: 65),
+                          child: UiHelper.customButton(() {}),
+                        ),
+                      ],
+                    ),
+                    UiHelper.customText(
+                      text: 'Amul Taaza Toned',
+                      color: Color(0xFF000000),
+                      fontweight: FontWeight.normal,
+                      fontsize: 10,
+                    ),
+
+                  ],
+                ),
+                const SizedBox(width: 20),
+                Column(
+                  children: [
+                    Stack(
+                      children: [
+                        UiHelper.customImage(img: 'tomato.png'),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 95, left: 65),
+                          child: UiHelper.customButton(() {}),
+                        ),
+                      ],
+                    ),
+                    UiHelper.customText(
+                      text: 'Amul Taaza Toned',
+                      color: Color(0xFF000000),
+                      fontweight: FontWeight.normal,
+                      fontsize: 10,
+                    ),
+                  ],
+                ),
+
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/repository/widget/uihelper.dart
+++ lib/repository/widget/uihelper.dart
@@ -22,4 +22,51 @@ class UiHelper {
       ),
     );
   }
+
+  static Widget customTextField(TextEditingController controller) {
+    return Container(
+      height: 46,
+      width: 400,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(10),
+        border: Border.all(color: Color(0xFFC5C5C5)),
+      ),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+          hintText: 'Search "ice-cream"',
+          hintStyle: TextStyle(color: Color(0xFF9C9C9C)),
+          focusedBorder: OutlineInputBorder(borderSide: BorderSide.none),
+          prefixIcon: Icon(Icons.search_outlined),
+          suffixIcon: Icon(Icons.mic),
+        ),
+      ),
+    );
+  }
+
+  static Widget customButton(VoidCallback onTap) {
+    return Container(
+      height: 18,
+      width: 30,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(4),
+        border: Border.all(color: Color(0xFF27AF34)),
+      ),
+      child: Center(
+        child: Text(
+          'Add',
+          style: TextStyle(color: Color(0xFF27AF34), fontSize: 8),
+        ),
+      ),
+    );
+  }
+
+  static Widget customTimer(){
+    return Image.asset('assets/images/timer 1.png');
+  }
+  static Widget timerText(){
+    return Text('16 Mins',style: TextStyle(fontSize: 12,color: Color(0xFF9C9C9C)),);
+  }
 }
